b13faaaf470dbcf8146b40107f574d15cba740eb
vivie <vivie@129.104.73.175>
2017-03-24 15:31:30 +0100

    compte

--- content/content_respovos.php
+++ content/content_respovos.php
@@ -1,45 +1,45 @@
 <?php
 if (isset($_SESSION["loggedIn"])) {
-echo "<h3>".'Rentre ton voyage'."</h3>";
-  $form_values_valid=false;
-            
-if(isset($_POST["id"]) && $_POST["id"] != "" &&
-   isset($_POST["nom"]) && $_POST["nom"] != "" && 
-   isset($_POST["section"]) && $_POST["section"] != "" && 
-   isset($_POST["promotion"]) && $_POST["promotion"] != "" &&
-   isset($_POST["latitude"]) && $_POST["latitude"] != "" &&
-   isset($_POST["longitude"]) && $_POST["longitude"] != "" &&
-   isset($_POST["information"]) && $_POST["information"] != "" ){
-   insererVoyage($_POST["id"],$_POST["nom"],$_POST["section"],$_POST["promotion"],$_POST["latitude"],$_POST["longitude"],$_POST["information"]);
-    $form_values_valid=true;
-    echo "Voyage Enregistré"; 
-}
- 
-if (!$form_values_valid) {
-printRegisterFormV();
-}
+    echo "<h3>" . 'Rentre ton voyage' . "</h3>";
+    $form_values_valid = false;
+
+    if (isset($_POST["id"]) && $_POST["id"] != "" &&
+            isset($_POST["nom"]) && $_POST["nom"] != "" &&
+            isset($_POST["section"]) && $_POST["section"] != "" &&
+            isset($_POST["promotion"]) && $_POST["promotion"] != "" &&
+            isset($_POST["latitude"]) && $_POST["latitude"] != "" &&
+            isset($_POST["longitude"]) && $_POST["longitude"] != "" &&
+            isset($_POST["information"]) && $_POST["information"] != "") {
+        insererVoyage($_POST["id"], $_POST["nom"], $_POST["section"], $_POST["promotion"], $_POST["latitude"], $_POST["longitude"], $_POST["information"]);
+        $form_values_valid = true;
+        echo "Voyage Enregistré";
+    }
+
+    if (!$form_values_valid) {
+        printRegisterFormV();
+    }
 }
 ?>
 
 
 <div id="floating-panel">
-      <input id="address" type="textbox" value="Sydney, NSW">
-      <input id="submit" type="button" value="Geocode">
+    <input id="address" type="textbox" value="Sydney, NSW">
+    <input id="submit" type="button" value="Geocode">
 </div>
 
 
 
 <div id="maCarte"></div>
 
- 
+
 
 <script>
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('maCarte'), {
-    zoom: 8,
-    center: {lat: -34.397, lng: 150.644}
-  });
-  var geocoder = new google.maps.Geocoder();
+    function initMap() {
+        var map = new google.maps.Map(document.getElementById('maCarte'), {
+            zoom: 8,
+            center: {lat: -34.397, lng: 150.644}
+        });
+        var geocoder = new google.maps.Geocoder();
 
   document.getElementById('submit').addEventListener('click', function() {
     geocodeAddress(geocoder, map);
